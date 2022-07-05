c6109f8591030099142430dc64d83a022201d490
HilarySmartt <hilary.smartt@gmail.com>
2022-07-05 06:51:12 -0700

    Task n-0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,13 +3,34 @@
 /**
  * main - Print _putchar
  * Return: Always 0
+ * Originally, the header file i used in main.h was
+ * int _putchar (char *)
+ * this is because i wanted my output to be
+ * a string of _putchar and i wont type so much
+ * and so my original description was
+ *
+ * char *x;
+ *
+ * x = "_putchar";
+ * printf("%s\n", x);
+ *
+ * and it worked
+ * but on getting to Task 1, i realised
+ * i still need to use the function _putchar and
+ * it can only print out a char
+ * so my code had to be modified to this...
+ *
  * by HilarySmartt
  */
 int main(void)
 {
-	char *x;
-
-	x = "_putchar";
-	printf("%s\n", x);
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
 	return (0);
 }
