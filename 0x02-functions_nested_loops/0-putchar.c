c2dff6cf454d2e10863e71c414fa5a275f2d1f56
HilarySmartt <hilary.smartt@gmail.com>
2022-07-05 05:20:18 -0700

    Task n-0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Print _putchar
+ * Return: Always 0
+ * by HilarySmartt
+ */
+int main(void)
+{
+	char *x;
+
+	x = "_putchar";
+	printf("%s\n", x);
+	return (0);
+}
