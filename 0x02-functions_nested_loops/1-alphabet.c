fca7b2e5d37bb0e889ac0f9fa3ee627dc3f7830c
HilarySmartt <hilary.smartt@gmail.com>
2022-07-05 15:31:43 -0700

    Task n-2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+/*
+ * File: 1-alphabet.c
+ * by HilarySmartt
+ */
+
+#include "main.h"
+
+
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
+
+void print_alphabet(void)
+{
+	char a;
+	
+	for (a = 97; a <= 122; a++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+}
