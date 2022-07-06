fa2937c86e118eb67f2a251a6b4eb4d2e97c3d4b
HilarySmartt <hilary.smartt@gmail.com>
2022-07-05 17:39:11 -0700

    Task n-2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+/*
+ * File: alphabet ten times
+ * Auth: HilarySmartt
+ * /
+
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times, in lowercase
+ */
+
+void print_alphabet_x10(void)
+
+{
+	int count = 0;
+	char a;
+
+	while (count++ <= 9)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+			_putchar(a);
+
+		_putchar('\n');
+	}
+}
