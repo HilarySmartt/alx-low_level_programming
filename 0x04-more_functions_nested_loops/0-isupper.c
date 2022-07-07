ab3ba8c431b73d7b018f32ad5b80523b0a2cdb8b
HilarySmartt <hilary.smartt@gmail.com>
2022-07-07 06:19:12 -0700

    Task m-0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * _isuper - returns 1 if input is uppercase and 0 if lowercase
+ * Return: true 1 or faslse 0
+ * @c: only input
+ * Auth: HilarySmartt
+ */
+
+int _isuper(int c)
+{
+	int boolean;
+
+	if ((c >= 65) || (c <= 90))
+	{
+		boolean = 1;
+	}
+	else
+	{
+		boolean = 0;
+	}
+
+	return (boolean);
+}
