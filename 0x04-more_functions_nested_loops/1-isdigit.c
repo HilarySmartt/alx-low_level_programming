dd5713456e59c73d3496628a571e3e63c80d6279
HilarySmartt <hilary.smartt@gmail.com>
2022-07-07 18:37:24 -0700

    Task m-2

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digits
+ * @c: digits to be checked
+ * Return: 1 or 0
+ * Auth: HilarySmartt
+ */
+
+int _isdigit(int c)
+{
+	int boolean;
+
+	if (c >= 0 a && c <= 9)
+		boolean = 1;
+	else
+		boolean = 0;
+
+	return (boolean);
+}
