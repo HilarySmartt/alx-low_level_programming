08656ae6c0533838542c26b7b2eff805e097416a
HilarySmartt <hilary.smartt@gmail.com>
2022-07-12 20:16:40 -0700

    Task pas-6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 -  prints every other character of a string, starting with the
+ * first character, followed by a new line
+ * @str: parameter to be printed in new lines
+ * Return: nothing:
+ */
+
+void puts2(char *str)
+{
+	int p;
+
+	for (p = 0; str[p] != '\0'; p++)
+		if (p % 2 == 0)
+			_putchar(str[p]);
+		_putchar('\n');
+}
