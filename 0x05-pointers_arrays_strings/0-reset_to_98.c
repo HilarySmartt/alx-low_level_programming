5fe150e8bd404053e079e50526c7bc61b0265bfe
HilarySmartt <hilary.smartt@gmail.com>
2022-07-12 18:59:27 -0700

    Task pas-0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the parameter integer to 98
+ * @n: the integer pointer parameter to be reset
+ * Return: nothing.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
