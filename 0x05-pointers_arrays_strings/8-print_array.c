7c23acb4d918cecfffcc3f81c8ae77a11696f573
HilarySmartt <hilary.smartt@gmail.com>
2022-07-12 20:45:43 -0700

    Task pas-8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_array - prints n elements of an array of integers,
@@ -14,7 +15,7 @@ void print_array(int *a, int n)
 
 	for (x = 0; x < n; x++)
 		if (x != n - 1)
-		printf("%d, ", a[x]);
+			printf("%d, ", a[x]);
 		else
 			printf("%d", a[x]);
 
